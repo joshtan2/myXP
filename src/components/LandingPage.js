1d31bfcbb063ee4bff1419e570041a5baab8b591
Joshua Tan <31526067+joshtan2@users.noreply.github.com>
2023-04-10 12:45:28 -0700

    AWS Implementation

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import { Auth } from 'aws-amplify';
 export default function LandingPage(props) {
-
+    async function checkUser(){
+        const user = await Auth.currentAuthenticatedUser()
+        console.log('user: ', user)
+    }
     return (
         <div>
-            {/* <p>Hello</p> */}
+            
+            <button onClick={()=>Auth.federatedSignIn({provider:"Goggle"})}>Sign in w google</button>
+            <button onClick={checkUser}>Check user</button>
         </div>
     )
 }
